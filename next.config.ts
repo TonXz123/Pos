5b3e4e1c50ce7cee42b29fe565cf2fa8e4d1796b
TonXz123 <phathorn2549@gmail.com>
2026-03-03 18:34:59 +0700

    fix: allow uploadthing images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io"
+      },
+      {
+        protocol: "https",
+        hostname: "ufs.sh"
+      }
+    ]
+  },
   async headers() {
     return [
       {
